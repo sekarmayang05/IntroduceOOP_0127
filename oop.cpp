cbb8de4a886003650896a04710ee0cf6756b6c11
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-04-29 19:05:59 +0700

    membuat procedure print data

--- oop.cpp
+++ oop.cpp
@@ -6,6 +6,13 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
+    
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+
 
 
 };
